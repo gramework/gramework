c665dbd860d552f778cea952b74725c67eedb848
Kirill Danshin <k@guava.by>
2017-02-13 10:16:10 +0300

    users: fix build

--- users/types.go
+++ users/types.go
@@ -2,11 +2,12 @@ package users
 
 import (
 	"github.com/gramework/threadsafe/cache"
+	"github.com/gramework/threadsafe/store"
 )
 
 type (
 	System struct {
-		store *Store
+		store *store.Store
 		cache *cache.Instance
 	}
 )
