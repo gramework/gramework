be56f7b16f0d8b1a3e060d5e33a8546e1e1509e9
Kirill Danshin <k@guava.by>
2017-02-13 10:10:06 +0300

    firewall: initial implementation

---
+++ firewall.go
@@ -0,0 +1,112 @@
+package gramework
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+func (app *App) initFirewall() {
+	go app.firewall.cleanupRequestsCountLoop()
+	go app.firewall.releaseBlockedRemoteAddrLoop()
+}
+
+// NewRequest tells the firewall, that a new request happened.
+// True is returned, if this request should be blocked,
+// because the IP is on the block list.
+// The remote address is always returned for logging purpose.
+func (fw *firewall) NewRequest(ctx *Context) (shouldBeBlocked bool, remoteAddr string) {
+	// Get the remote addresse of the request
+	remoteAddr = ctx.RemoteIP().String()
+
+	// Check if this remote address is blocked
+	if fw.isBlocked(remoteAddr) {
+		return true, remoteAddr
+	}
+
+	// Register the new request in a new goroutine
+	go fw.addRequest(remoteAddr)
+
+	return false, remoteAddr
+}
+
+// isBlocked checks if the remote addres is blocked
+func (fw *firewall) isBlocked(remoteAddr string) bool {
+	fw.blockListMutex.Lock()
+
+	// Check if the remote addresse exists in the blocked map
+	_, exists := fw.blockList[remoteAddr]
+
+	fw.blockListMutex.Unlock()
+	return exists
+}
+
+func (fw *firewall) addRequest(remoteAddr string) {
+	// Lock the mutex
+	fw.requestCounterMutex.Lock()
+	defer fw.requestCounterMutex.Unlock()
+
+	count, ok := fw.requestCounter[remoteAddr]
+	if !ok {
+		count = 1
+	}
+
+	// Add the remote address to the block map, if the count
+	// reached the limit.
+	if count > atomic.LoadInt64(fw.MaxReqPerMin) {
+		// Remove the remote address from the request counter map
+		delete(fw.requestCounter, remoteAddr)
+
+		// Get the current timestamp
+		timestamp := time.Now().Unix()
+
+		// Lock the mutex
+		fw.blockListMutex.Lock()
+		defer fw.blockListMutex.Unlock()
+
+		// Add the remote address with the timestamp to the block map
+		fw.blockList[remoteAddr] = timestamp
+
+		return
+	}
+
+	// Save the incremented count to the map
+	fw.requestCounter[remoteAddr] = count + 1
+}
+
+func (fw *firewall) cleanupRequestsCountLoop() {
+	for {
+		time.Sleep(time.Minute)
+
+		fw.requestCounterMutex.Lock()
+
+		// Clear the map if not emtpy
+		if len(fw.requestCounter) > 0 {
+			fw.requestCounter = make(map[string]int64)
+		}
+		fw.requestCounterMutex.Unlock()
+	}
+}
+
+func (fw *firewall) releaseBlockedRemoteAddrLoop() {
+	for {
+		time.Sleep(time.Minute)
+
+		fw.blockListMutex.Lock()
+
+		if len(fw.blockList) == 0 {
+			// nothing to do here
+			fw.blockListMutex.Unlock()
+			return
+		}
+
+		releaseTimestamp := time.Now().Unix() - int64(atomic.LoadInt64(fw.BlockTimeout))
+
+		// removing expired blocks
+		for key, timestamp := range fw.blockList {
+			if timestamp < releaseTimestamp {
+				delete(fw.blockList, key)
+			}
+		}
+		fw.blockListMutex.Unlock()
+	}
+}
--- types.go
+++ types.go
@@ -1,8 +1,11 @@
 package gramework
 
 import (
+	"sync"
+
 	"github.com/apex/log"
 	"github.com/buaazp/fasthttprouter"
+	"github.com/gramework/utils/nocopy"
 	"github.com/valyala/fasthttp"
 )
 
@@ -11,12 +14,44 @@ type (
 	App struct {
 		router       *fasthttprouter.Router
 		errorHandler func(func(*fasthttp.RequestCtx) error)
-		Logger       log.Interface
+		firewall     *firewall
+
+		Logger    log.Interface
+		TLSEmails []string
+		Settings  Settings
 	}
 
 	// Context is a gramework request context
 	Context struct {
 		*fasthttp.RequestCtx
 		Logger log.Interface
+		nocopy nocopy.NoCopy
+	}
+
+	// Settings for an App instance
+	Settings struct {
+		Firewall FirewallSettings
+	}
+
+	// FirewallSettings represents a new firewall settings.
+	// Internal firewall representation copies this settings
+	// atomically.
+	FirewallSettings struct {
+		// MaxReqPerMin is a max request per minute count
+		MaxReqPerMin int64
+		// BlockTimeout in seconds
+		BlockTimeout int64
+	}
+
+	firewall struct {
+		// Store a copy of current settings
+		MaxReqPerMin *int64
+		BlockTimeout *int64
+
+		blockList      map[string]int64
+		blockListMutex sync.Mutex
+
+		requestCounter      map[string]int64
+		requestCounterMutex sync.Mutex
 	}
 )
