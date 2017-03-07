fad956767b83f8f98964e5d52c570e02de169c9d
Kirill Danshin <k@guava.by>
2017-03-07 17:41:10 +0300

    app:listenAndServe: make address argument optional
    Signed-off-by: Kirill Danshin <k@guava.by>

--- app_listenAndServe.go
+++ app_listenAndServe.go
@@ -1,10 +1,36 @@
 package gramework
 
 import (
+	"errors"
+	"flag"
+
 	"github.com/valyala/fasthttp"
 )
 
 // ListenAndServe on given addr
-func (app *App) ListenAndServe(addr string) error {
-	return fasthttp.ListenAndServe(addr, app.router.Handler)
+func (app *App) ListenAndServe(addr ...string) error {
+	var bind string
+	if len(addr) > 0 {
+		bind = addr[0]
+	} else {
+		if !app.flagsRegistered {
+			app.RegFlags()
+		}
+	}
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	if app.Flags.values != nil {
+		if bindFlag, ok := app.Flags.values["bind"]; ok {
+			bind = *bindFlag.Value
+		}
+	}
+	if bind == "" {
+		return errors.New("No bind address provided")
+	}
+	l := app.Logger.WithField("bind", bind)
+	l.Info("Starting")
+	err := fasthttp.ListenAndServe(bind, app.router.Handler)
+	l.Errorf("ListenAndServe failed: %s", err)
+	return err
 }
