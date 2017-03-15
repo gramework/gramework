c51e0eb9983a1815c4d7f9d0bc9f21f76f5755e9
Kirill Danshin <k@guava.by>
2017-03-16 01:31:11 +0300

    app: test New()
    Signed-off-by: Kirill Danshin <k@guava.by>

---
+++ new_test.go
@@ -0,0 +1,32 @@
+package gramework
+
+import "testing"
+
+func TestNewShouldNeverReturnNil(t *testing.T) {
+	app := New()
+	if app == nil {
+		t.Log("App is nil!")
+		t.FailNow()
+		return
+	}
+	if app.defaultRouter == nil {
+		t.Log("App Router is nil!")
+		t.FailNow()
+		return
+	}
+	if app.Logger == nil {
+		t.Log("App Logger is nil!")
+		t.FailNow()
+		return
+	}
+	if app.domainListLock == nil {
+		t.Log("App domain list lock is nil!")
+		t.FailNow()
+		return
+	}
+	if app.firewall == nil {
+		t.Log("App firewall is nil!")
+		t.FailNow()
+		return
+	}
+}
