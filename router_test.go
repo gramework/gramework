463be8fc36a7a2d9762da81db44a8860eef3a6e1
Kirill Danshin <k@guava.by>
2017-03-16 05:33:05 +0300

    router: domain router, http(s) router tests
    Signed-off-by: Kirill Danshin <k@guava.by>

--- router_test.go
+++ router_test.go
@@ -160,3 +160,483 @@ func TestRouter(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestDomainRouter(t *testing.T) {
+	app := New()
+	r := app.Domain("example.com")
+	if h, _ := r.Lookup("GET", "/", nil); h != nil {
+		t.Log("GET / should not return handler before registration")
+		t.FailNow()
+	}
+	r.GET("/", 1)
+	if h, _ := r.Lookup("GET", "/", nil); h == nil {
+		t.Log("GET / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("GET", "/abc", nil); h != nil {
+		t.Log("GET /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.GET("/abc", "abc")
+
+	if h, _ := r.Lookup("GET", "/abc", nil); h == nil {
+		t.Log("GET /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	r.GET("/redir", app.ToTLSHandler())
+
+	// POST
+
+	if h, _ := r.Lookup("POST", "/", nil); h != nil {
+		t.Log("POST / should not return handler before registration")
+		t.FailNow()
+	}
+	r.POST("/", 1)
+	if h, _ := r.Lookup("POST", "/", nil); h == nil {
+		t.Log("POST / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("POST", "/abc", nil); h != nil {
+		t.Log("POST /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.POST("/abc", "abc")
+
+	if h, _ := r.Lookup("POST", "/abc", nil); h == nil {
+		t.Log("POST /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PUT
+	if h, _ := r.Lookup("PUT", "/", nil); h != nil {
+		t.Log("PUT / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PUT("/", 1)
+	if h, _ := r.Lookup("PUT", "/", nil); h == nil {
+		t.Log("PUT / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PUT", "/abc", nil); h != nil {
+		t.Log("PUT /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PUT("/abc", "abc")
+
+	if h, _ := r.Lookup("PUT", "/abc", nil); h == nil {
+		t.Log("PUT /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// DELETE
+	if h, _ := r.Lookup("DELETE", "/", nil); h != nil {
+		t.Log("DELETE / should not return handler before registration")
+		t.FailNow()
+	}
+	r.DELETE("/", 1)
+	if h, _ := r.Lookup("DELETE", "/", nil); h == nil {
+		t.Log("DELETE / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h != nil {
+		t.Log("DELETE /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.DELETE("/abc", "abc")
+
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h == nil {
+		t.Log("DELETE /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// HEAD
+	if h, _ := r.Lookup("HEAD", "/", nil); h != nil {
+		t.Log("HEAD / should not return handler before registration")
+		t.FailNow()
+	}
+	r.HEAD("/", 1)
+	if h, _ := r.Lookup("HEAD", "/", nil); h == nil {
+		t.Log("HEAD / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h != nil {
+		t.Log("HEAD /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.HEAD("/abc", "abc")
+
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h == nil {
+		t.Log("HEAD /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// OPTIONS
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h != nil {
+		t.Log("OPTIONS / should not return handler before registration")
+		t.FailNow()
+	}
+	r.OPTIONS("/", 1)
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h == nil {
+		t.Log("OPTIONS / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h != nil {
+		t.Log("OPTIONS /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.OPTIONS("/abc", "abc")
+
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h == nil {
+		t.Log("OPTIONS /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PATCH
+	if h, _ := r.Lookup("PATCH", "/", nil); h != nil {
+		t.Log("PATCH / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PATCH("/", 1)
+	if h, _ := r.Lookup("PATCH", "/", nil); h == nil {
+		t.Log("PATCH / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h != nil {
+		t.Log("PATCH /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PATCH("/abc", "abc")
+
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h == nil {
+		t.Log("PATCH /abc should return handler after registration")
+		t.FailNow()
+	}
+}
+
+func TestDomainHTTPRouter(t *testing.T) {
+	app := New()
+	r := app.Domain("example.com").HTTP()
+	if h, _ := r.Lookup("GET", "/", nil); h != nil {
+		t.Log("GET / should not return handler before registration")
+		t.FailNow()
+	}
+	r.GET("/", 1)
+	if h, _ := r.Lookup("GET", "/", nil); h == nil {
+		t.Log("GET / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("GET", "/abc", nil); h != nil {
+		t.Log("GET /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.GET("/abc", "abc")
+
+	if h, _ := r.Lookup("GET", "/abc", nil); h == nil {
+		t.Log("GET /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	r.GET("/redir", app.ToTLSHandler())
+
+	// POST
+
+	if h, _ := r.Lookup("POST", "/", nil); h != nil {
+		t.Log("POST / should not return handler before registration")
+		t.FailNow()
+	}
+	r.POST("/", 1)
+	if h, _ := r.Lookup("POST", "/", nil); h == nil {
+		t.Log("POST / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("POST", "/abc", nil); h != nil {
+		t.Log("POST /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.POST("/abc", "abc")
+
+	if h, _ := r.Lookup("POST", "/abc", nil); h == nil {
+		t.Log("POST /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PUT
+	if h, _ := r.Lookup("PUT", "/", nil); h != nil {
+		t.Log("PUT / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PUT("/", 1)
+	if h, _ := r.Lookup("PUT", "/", nil); h == nil {
+		t.Log("PUT / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PUT", "/abc", nil); h != nil {
+		t.Log("PUT /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PUT("/abc", "abc")
+
+	if h, _ := r.Lookup("PUT", "/abc", nil); h == nil {
+		t.Log("PUT /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// DELETE
+	if h, _ := r.Lookup("DELETE", "/", nil); h != nil {
+		t.Log("DELETE / should not return handler before registration")
+		t.FailNow()
+	}
+	r.DELETE("/", 1)
+	if h, _ := r.Lookup("DELETE", "/", nil); h == nil {
+		t.Log("DELETE / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h != nil {
+		t.Log("DELETE /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.DELETE("/abc", "abc")
+
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h == nil {
+		t.Log("DELETE /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// HEAD
+	if h, _ := r.Lookup("HEAD", "/", nil); h != nil {
+		t.Log("HEAD / should not return handler before registration")
+		t.FailNow()
+	}
+	r.HEAD("/", 1)
+	if h, _ := r.Lookup("HEAD", "/", nil); h == nil {
+		t.Log("HEAD / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h != nil {
+		t.Log("HEAD /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.HEAD("/abc", "abc")
+
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h == nil {
+		t.Log("HEAD /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// OPTIONS
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h != nil {
+		t.Log("OPTIONS / should not return handler before registration")
+		t.FailNow()
+	}
+	r.OPTIONS("/", 1)
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h == nil {
+		t.Log("OPTIONS / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h != nil {
+		t.Log("OPTIONS /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.OPTIONS("/abc", "abc")
+
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h == nil {
+		t.Log("OPTIONS /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PATCH
+	if h, _ := r.Lookup("PATCH", "/", nil); h != nil {
+		t.Log("PATCH / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PATCH("/", 1)
+	if h, _ := r.Lookup("PATCH", "/", nil); h == nil {
+		t.Log("PATCH / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h != nil {
+		t.Log("PATCH /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PATCH("/abc", "abc")
+
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h == nil {
+		t.Log("PATCH /abc should return handler after registration")
+		t.FailNow()
+	}
+}
+
+func TestDomainHTTPSRouter(t *testing.T) {
+	app := New()
+	r := app.Domain("example.com").HTTPS()
+	if h, _ := r.Lookup("GET", "/", nil); h != nil {
+		t.Log("GET / should not return handler before registration")
+		t.FailNow()
+	}
+	r.GET("/", 1)
+	if h, _ := r.Lookup("GET", "/", nil); h == nil {
+		t.Log("GET / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("GET", "/abc", nil); h != nil {
+		t.Log("GET /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.GET("/abc", "abc")
+
+	if h, _ := r.Lookup("GET", "/abc", nil); h == nil {
+		t.Log("GET /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	r.GET("/redir", app.ToTLSHandler())
+
+	// POST
+
+	if h, _ := r.Lookup("POST", "/", nil); h != nil {
+		t.Log("POST / should not return handler before registration")
+		t.FailNow()
+	}
+	r.POST("/", 1)
+	if h, _ := r.Lookup("POST", "/", nil); h == nil {
+		t.Log("POST / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("POST", "/abc", nil); h != nil {
+		t.Log("POST /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.POST("/abc", "abc")
+
+	if h, _ := r.Lookup("POST", "/abc", nil); h == nil {
+		t.Log("POST /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PUT
+	if h, _ := r.Lookup("PUT", "/", nil); h != nil {
+		t.Log("PUT / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PUT("/", 1)
+	if h, _ := r.Lookup("PUT", "/", nil); h == nil {
+		t.Log("PUT / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PUT", "/abc", nil); h != nil {
+		t.Log("PUT /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PUT("/abc", "abc")
+
+	if h, _ := r.Lookup("PUT", "/abc", nil); h == nil {
+		t.Log("PUT /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// DELETE
+	if h, _ := r.Lookup("DELETE", "/", nil); h != nil {
+		t.Log("DELETE / should not return handler before registration")
+		t.FailNow()
+	}
+	r.DELETE("/", 1)
+	if h, _ := r.Lookup("DELETE", "/", nil); h == nil {
+		t.Log("DELETE / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h != nil {
+		t.Log("DELETE /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.DELETE("/abc", "abc")
+
+	if h, _ := r.Lookup("DELETE", "/abc", nil); h == nil {
+		t.Log("DELETE /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// HEAD
+	if h, _ := r.Lookup("HEAD", "/", nil); h != nil {
+		t.Log("HEAD / should not return handler before registration")
+		t.FailNow()
+	}
+	r.HEAD("/", 1)
+	if h, _ := r.Lookup("HEAD", "/", nil); h == nil {
+		t.Log("HEAD / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h != nil {
+		t.Log("HEAD /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.HEAD("/abc", "abc")
+
+	if h, _ := r.Lookup("HEAD", "/abc", nil); h == nil {
+		t.Log("HEAD /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// OPTIONS
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h != nil {
+		t.Log("OPTIONS / should not return handler before registration")
+		t.FailNow()
+	}
+	r.OPTIONS("/", 1)
+	if h, _ := r.Lookup("OPTIONS", "/", nil); h == nil {
+		t.Log("OPTIONS / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h != nil {
+		t.Log("OPTIONS /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.OPTIONS("/abc", "abc")
+
+	if h, _ := r.Lookup("OPTIONS", "/abc", nil); h == nil {
+		t.Log("OPTIONS /abc should return handler after registration")
+		t.FailNow()
+	}
+
+	// PATCH
+	if h, _ := r.Lookup("PATCH", "/", nil); h != nil {
+		t.Log("PATCH / should not return handler before registration")
+		t.FailNow()
+	}
+	r.PATCH("/", 1)
+	if h, _ := r.Lookup("PATCH", "/", nil); h == nil {
+		t.Log("PATCH / should return handler after registration")
+		t.FailNow()
+	}
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h != nil {
+		t.Log("PATCH /abc should not return handler before registration")
+		t.FailNow()
+	}
+
+	r.PATCH("/abc", "abc")
+
+	if h, _ := r.Lookup("PATCH", "/abc", nil); h == nil {
+		t.Log("PATCH /abc should return handler after registration")
+		t.FailNow()
+	}
+}
