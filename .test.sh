0daef36e4c644ed1b47c758c863e662269403b0b
Kirill Danshin <k@guava.by>
2017-03-20 12:27:57 +0300

    ci: try to run with
    Signed-off-by: Kirill Danshin <k@guava.by>

--- .test.sh
+++ .test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
+    sudo GOPATH=$GOPATH GOROOT=$GOROOT `where go` test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
