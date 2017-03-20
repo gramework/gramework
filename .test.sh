03162fc073f7047731767c92aead7d1c985e73d0
Kirill Danshin <k@guava.by>
2017-03-20 12:21:55 +0300

    ci: fix
    Signed-off-by: Kirill Danshin <k@guava.by>

--- .test.sh
+++ .test.sh
@@ -5,7 +5,7 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
 	# we're going to listen on :443, so we require sudo for testing purposes
-    sudo GOPATH=$GOPATH GOROOT=$GOROOT go test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
+    go test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
