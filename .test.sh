157ebd35827221b42e24c746d32547ea1cd056f8
Kirill Danshin <k@guava.by>
2017-03-20 12:40:02 +0300

    ci: fix tests stuck on rm
    Signed-off-by: Kirill Danshin <k@guava.by>

--- .test.sh
+++ .test.sh
@@ -7,6 +7,6 @@ for d in $(go list ./... | grep -v vendor); do
     sudo GOPATH=$GOPATH GOROOT=$GOROOT `which go` test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
-        rm profile.out
+        rm -rf profile.out
     fi
 done
