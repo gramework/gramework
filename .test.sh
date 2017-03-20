608cb7431a23e34fcd942acae9effda05c7645c3
Kirill Danshin <k@guava.by>
2017-03-20 12:24:15 +0300

    ci: remove comment
    Signed-off-by: Kirill Danshin <k@guava.by>

--- .test.sh
+++ .test.sh
@@ -4,7 +4,6 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-	# we're going to listen on :443, so we require sudo for testing purposes
     go test  -bench=. -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
