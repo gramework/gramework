d128b54ec6773a2f5483760368bb28b27681b00d
Kirill Danshin <k@guava.by>
2017-03-28 18:13:58 +0300

    nanotime: workaround compiler bug
    Signed-off-by: Kirill Danshin <k@guava.by>

--- nanotime.go
+++ nanotime.go
@@ -3,7 +3,10 @@ package gramework
 import _ "unsafe" // required to use //go:linkname
 
 // Nanotime is monotonic time provider.
-//
+func Nanotime() int64 {
+	return nanotime()
+}
+
 //go:noescape
 //go:linkname Nanotime runtime.nanotime
-func Nanotime() int64
+func nanotime() int64
