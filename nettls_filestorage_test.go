70eda4e0b430de894f5cd9f15a18f90909004735
Kirill Danshin <k@guava.by>
2017-04-04 10:22:20 +0300

    nettls_: temporary unsupported
    Signed-off-by: Kirill Danshin <k@guava.by>

--- nettls_filestorage_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2015-2017 Matt Holt and caddy contributors
-// Copyright 2017 Kirill Danshin and gramework contributors
-
-package gramework
-
-// *********************************** NOTE ********************************
-// Due to circular package dependencies with the storagetest sub package and
-// the fact that we want to use that harness to test file storage, the tests
-// for file storage are done in the storagetest package.
