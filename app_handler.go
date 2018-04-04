cc244b1fd44f81e42f65f85362c78b42d8f0eba5
Maxim Lebedev <git@toby3d.ru>
2018-04-04 13:47:16 +0500

    :rotating_light: Removed some linter warnings
    merging variables, struct size, identical expressions and etc.

--- app_handler.go
+++ app_handler.go
@@ -37,11 +37,10 @@ func (app *App) handler() func(*fasthttp.RequestCtx) {
 		}
 		app.middlewaresMu.RUnlock()
 		if len(app.domains) > 0 {
-			d := string(ctx.URI().Host())
 			app.domainListLock.RLock()
-			if app.domains[d] != nil {
+			if app.domains[string(ctx.URI().Host())] != nil {
 				app.domainListLock.RUnlock()
-				app.domains[d].Handler()(ctx)
+				app.domains[string(ctx.URI().Host())].Handler()(ctx)
 				app.runMiddlewaresAfterRequest(ctx)
 				ctx.saveCookies()
 				return
