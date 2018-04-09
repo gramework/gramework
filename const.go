e54b23edac2532aa7c08709358adca74b36ab7fb
Maxim Lebedev <git@toby3d.ru>
2018-04-09 12:18:50 +0500

    :art: Constants format and use values from net/http package

--- const.go
+++ const.go
@@ -1,26 +1,30 @@
 package gramework
 
+import (
+	"net/http"
+)
+
 const (
 	// MethodDELETE is the HTTP DELETE method
-	MethodDELETE = "DELETE"
+	MethodDELETE = http.MethodDelete
 
 	// MethodGET is the HTTP GET method
-	MethodGET = "GET"
+	MethodGET = http.MethodGet
 
 	// MethodHEAD is the HTTP HEAD method
-	MethodHEAD = "HEAD"
+	MethodHEAD = http.MethodHead
 
 	// MethodOPTIONS is the HTTP OPTIONS method
-	MethodOPTIONS = "OPTIONS"
+	MethodOPTIONS = http.MethodOptions
 
 	// MethodPATCH is the HTTP PATCH method
-	MethodPATCH = "PATCH"
+	MethodPATCH = http.MethodPatch
 
 	// MethodPOST is the HTTP POST method
-	MethodPOST = "POST"
+	MethodPOST = http.MethodPost
 
 	// MethodPUT is the HTTP PUT method
-	MethodPUT = "PUT"
+	MethodPUT = http.MethodPut
 )
 
 const (
@@ -28,10 +32,29 @@ const (
 
 	fmtV = "%v"
 
-	htmlCT = "text/html; charset=utf8"
-
+	htmlCT  = "text/html; charset=utf8"
+	jsonCT  = "application/json;charset=utf8"
 	xmlCT   = "text/xml"
 	plainCT = "text/plain"
 
 	acceptHeader = "Accept"
+)
+
+const (
+	badRequest                        = "Bad Request"
+	corsAccessControlAllowCredentials = "Access-Control-Allow-Credentials"
+	corsAccessControlAllowHeaders     = "Access-Control-Allow-Headers"
+	corsAccessControlAllowMethods     = "Access-Control-Allow-Methods"
+	corsAccessControlAllowOrigin      = "Access-Control-Allow-Origin"
+	corsCType                         = "Content-Type, *"
+	forbidden                         = "Forbidden"
+	hOrigin                           = "Origin"
+	https                             = "https"
+	methods                           = "GET,PUT,POST,DELETE"
+	trueStr                           = "true"
+	forbiddenCode                     = http.StatusForbidden
+	redirectCode                      = http.StatusMovedPermanently
+	temporaryRedirectCode             = http.StatusTemporaryRedirect
+	one                               = 1
+	zero                              = 0
 )
