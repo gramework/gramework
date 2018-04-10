070e57c45ff2dd4b4e33d988cc2f4b0725e96b82
Maxim Lebedev <git@toby3d.ru>
2018-04-10 11:58:33 +0500

    :truck: Moved grypto test to test directory

--- test/grypto_test.go
+++ test/grypto_test.go
@@ -1,17 +1,22 @@
-package grypto
+package test
 
 import (
 	"testing"
 
 	"github.com/gramework/utils/grand"
+	"github.com/toby3d/gramework/grypto"
 	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	cost = 10
 )
 
 // TestSalt128 makes sure the result is always 32 hex characters
 func TestSalt128(t *testing.T) {
 	var salt []byte
 	for i := 0; i < 1024; i++ {
-		salt = Salt128()
+		salt = grypto.Salt128()
 		if len(salt) != 16 {
 			t.Errorf("Salt128 fail to generate 128 bit salt: %02x", salt)
 			t.FailNow()
@@ -28,14 +33,14 @@ func TestPasswordSanity(t *testing.T) {
 	for i := 0; i < 32; i++ {
 		grand.Read(pw)
 		grand.Read(pw2)
-		hash = PasswordHash(pw)
-		hash2 = PasswordHash(pw2)
+		hash = grypto.PasswordHash(pw)
+		hash2 = grypto.PasswordHash(pw2)
 
-		if !PasswordValid(hash, pw) {
+		if !grypto.PasswordValid(hash, pw) {
 			t.Errorf("PasswordValid should return true for the pair: %s and %s", hash, pw)
 			t.FailNow()
 		}
-		if PasswordValid(hash2, pw) {
+		if grypto.PasswordValid(hash2, pw) {
 			t.Error("PasswordValid is giving false positive")
 			t.FailNow()
 		}
@@ -51,14 +56,14 @@ func TestPasswordStringSanity(t *testing.T) {
 	for i := 0; i < 32; i++ {
 		grand.Read(pw)
 		grand.Read(pw2)
-		hash = PasswordHashString(string(pw))
-		hash2 = PasswordHashString(string(pw2))
+		hash = grypto.PasswordHashString(string(pw))
+		hash2 = grypto.PasswordHashString(string(pw2))
 
-		if !PasswordValid(hash, pw) {
+		if !grypto.PasswordValid(hash, pw) {
 			t.Errorf("PasswordValid should return true for the pair: %s and %s", hash, pw)
 			t.FailNow()
 		}
-		if PasswordValid(hash2, pw) {
+		if grypto.PasswordValid(hash2, pw) {
 			t.Error("PasswordValid is giving false positive")
 			t.FailNow()
 		}
@@ -71,7 +76,7 @@ func TestPasswordNeedsRehash(t *testing.T) {
 	for i := 0; i < 32; i++ {
 		grand.Read(pw)
 		hash, _ := bcrypt.GenerateFromPassword(pw, cost-1)
-		if !PasswordNeedsRehash(hash) {
+		if !grypto.PasswordNeedsRehash(hash) {
 			t.Errorf("PasswordNeedsRehash returned false, expected true")
 			t.FailNow()
 		}
@@ -79,7 +84,7 @@ func TestPasswordNeedsRehash(t *testing.T) {
 	for i := 0; i < 32; i++ {
 		grand.Read(pw)
 		hash, _ := bcrypt.GenerateFromPassword(pw, cost)
-		if PasswordNeedsRehash(hash) {
+		if grypto.PasswordNeedsRehash(hash) {
 			t.Errorf("PasswordNeedsRehash returned true, expected false")
 		}
 	}
@@ -96,8 +101,8 @@ func BenchmarkPassHashAndValidation(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i += 2 {
-		hash := PasswordHash(pws[i])
-		if !PasswordValid(hash, pws[i]) {
+		hash := grypto.PasswordHash(pws[i])
+		if !grypto.PasswordValid(hash, pws[i]) {
 			b.Errorf("PasswordValid should return true for the pair: %s and %s", hash, pws[i])
 			b.FailNow()
 		}
@@ -115,7 +120,7 @@ func BenchmarkPassHash(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		hash := PasswordHash(pws[i])
+		hash := grypto.PasswordHash(pws[i])
 		_ = hash
 	}
 }
