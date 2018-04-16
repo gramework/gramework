12f1a3d0880a2802af2c62299afa99b03cb0fd0f
Kirill Danshin <kirill@danshin.pro>
2018-04-17 02:14:44 +0300

    :page_facing_up: add licence header

--- context_logHeaders.go
+++ context_logHeaders.go
@@ -1,6 +1,15 @@
+// Copyright 2017 Kirill Danshin and Gramework contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+
 package gramework
 
-// LogHeaders for debug
+// LogHeaders logs all request headers for debug
 func (ctx *Context) LogHeaders() {
 	ctx.Request.Header.VisitAll(func(k, v []byte) {
 		ctx.Logger.Debugf("%s = [%s]\n", k, v)
