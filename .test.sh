74cd6ed67e9a3335c8ce1bcde9be5a52dc6d3dff
Kirill Danshin <k@guava.by>
2018-08-24 02:19:28 +0300

    fix .test.sh lint issue
    Signed-off-by: Kirill Danshin <k@guava.by>

--- .test.sh
+++ .test.sh
@@ -7,7 +7,7 @@ LIST=$(go list ./... | grep -v vendor)
 echo LIST: ${LIST}
 
 for d in $(go list ./... | grep -v vendor); do
-	sudo GOPATH=$GOPATH GOROOT=$GOROOT `which go` test -tags=letsstage -bench=. -short=$GRAMEWORK_SHORT -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
+	sudo GOPATH=$GOPATH GOROOT=$GOROOT $(which go) test -tags=letsstage -bench=. -short=$GRAMEWORK_SHORT -benchmem -v -race -coverprofile=profile.out -covermode=atomic $d
 	if [ -f profile.out ]; then
 		cat profile.out >> coverage.txt
 		rm -rf profile.out
